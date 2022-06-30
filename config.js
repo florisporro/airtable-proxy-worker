3ec2c4761da1c4207b200b49706028bbef7fa6c7
Floris Porro <floris@hologfx.io>
2022-06-30 22:07:10 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-	airtableApiUrl: "https://api.airtable.com",
-	airtableBaseId: "",
-	airtableApiVersion: "v0",
-	airtableApiKey: "",
-	cacheTime: 0,
-	prefix: "",
-	allowedTargets: "*",
-};
