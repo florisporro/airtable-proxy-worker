f68bc2bf588521b62555928f4a71bca0144b43c5
Floris Porro <floris@hologfx.io>
2022-06-30 14:59:54 +0200

    Expose new routing to handleRequest

--- src/handler.js
+++ src/handler.js
@@ -1,64 +1,60 @@
-import { isAllowed } from "./is-allowed.js";
-import { getMethod } from "./get-method.js";
-import { getTarget } from "./get-target.js";
-import { config } from "../config.js";
+import { getRouting } from "./routing.js";
+import { config, routes } from "../config.js";
 
-const parseJson = (obj) => {
-	try {
-		return JSON.parse(obj);
-	} catch (e) {
-		return obj;
-	}
-};
+export async function handleRequest(event) {
+	const { request } = event;
+	const { route, method, id } = getRouting(request, routes);
 
-export async function handleRequest(req) {
-	try {
-		const method = getMethod(req);
-		const target = getTarget(req, config);
-
-		if (
-			!isAllowed(
-				method,
-				target.airtableResource,
-				parseJson(config.allowedTargets)
-			)
-		) {
-			return new Response("Method Not Allowed", {
-				status: 405,
-				statusText: `Method "${method}" not allowed on "${
-					target.airtableResource
-				}" resource`,
-			});
-		}
-
-		const response = await fetch(target.airtableRequestUrl, {
-			headers: {
-				Authorization: `Bearer ${config.airtableApiKey}`,
-				"Content-type": "application/json",
-			},
-			method: method,
-			body: req.body,
-		});
-
-		const body = await response.body;
-
-		const headers = new Headers();
-		for (const kv of response.headers.entries()) {
-			headers.append(kv[0], kv[1]);
-		}
-		headers.set("Cache-Control", "max-age=" + config.cacheTime);
-
-		return new Response(body, {
-			status: response.status,
-			statusText: response.statusText,
-			headers: headers,
-		});
-	} catch (e) {
-		console.error(e);
-
-		return new Response("Bad Request", {
-			status: 400,
-			statusText: "Bad Request",
+	if (route === undefined) {
+		return new Response("Not found", {
+			status: 404,
+			statusText: "That route was not found on this API",
 		});
 	}
+
+	if (route.methods[method] === undefined) {
+		return new Response("Method Not Allowed", {
+			status: 405,
+			statusText: `Method "${method}" not allowed on the ${
+				route.name
+			} resource`,
+		});
+	}
+
+	return new Response(JSON.stringify({ route, method, id }), {
+		status: 200,
+		statusText: `We are developing :)`,
+	});
+
+	// try {
+	// 	const response = await fetch(target.airtableRequestUrl, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${config.airtableApiKey}`,
+	// 			"Content-type": "application/json",
+	// 		},
+	// 		method: method,
+	// 		body: req.body,
+	// 	});
+
+	// 	const body = await response.body;
+
+	// 	const headers = new Headers();
+	// 	for (const kv of response.headers.entries()) {
+	// 		headers.append(kv[0], kv[1]);
+	// 	}
+	// 	headers.set("Cache-Control", "max-age=" + config.cacheTime);
+
+	// 	return new Response(body, {
+	// 		status: response.status,
+	// 		statusText: response.statusText,
+	// 		headers: headers,
+	// 	});
+	// } catch (e) {
+	// 	console.error(e);
+
+	// 	return new Response("Bad Request", {
+	// 		status: 400,
+	// 		statusText: "Bad Request",
+	// 	});
+	// }
 }
